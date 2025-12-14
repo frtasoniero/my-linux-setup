0456335705eeaf8d69e8217839338710a438cced
felipe <felipe@debian.felipe>
2025-12-14 18:45:51 -0300

    update treesitter plugin to a newer version

--- nvim-config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim-config/nvim/lua/plugins/nvim-treesitter.lua
@@ -11,44 +11,64 @@ return {
 	event = { "BufReadPost", "BufNewFile" },
 	lazy = false,
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- language parsers that MUST be installed
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"css",
-				"dockerfile",
-				"go",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"rust",
-				"svelte",
-				"typescript",
-				"vue",
-				"yaml",
-			},
-			auto_install = true, -- auto-install any other parsers on opening new language files
-			sync_install = false,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<CR>",
-					node_incremental = "<CR>",
-					scope_incremental = "<TAB>",
-					node_decremental = "<S-TAB>",
-				},
-			},
+		-- language parsers that MUST be installed
+		local ensure_installed = {
+			"bash",
+			"c",
+			"cpp",
+			"css",
+			"dockerfile",
+			"go",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"rust",
+			"svelte",
+			"typescript",
+			"vue",
+			"yaml",
+		}
+
+		-- Install parsers
+		local installed = require("nvim-treesitter.install")
+		installed.prefer_git = true
+
+		-- Ensure parsers are installed
+		for _, lang in ipairs(ensure_installed) do
+			local ok, parser = pcall(vim.treesitter.language.inspect, lang)
+			if not ok then
+				vim.cmd("TSInstall " .. lang)
+			end
+		end
+
+		-- Enable highlighting
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
 		})
+
+		-- Enable indentation
+		vim.opt.foldmethod = "expr"
+		vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.opt.foldenable = false
+
+		-- Incremental selection keymaps
+		vim.keymap.set("n", "<CR>", function()
+			require("nvim-treesitter.incremental_selection").init_selection()
+		end, { desc = "Init treesitter selection" })
+		vim.keymap.set("v", "<CR>", function()
+			require("nvim-treesitter.incremental_selection").node_incremental()
+		end, { desc = "Increment treesitter selection" })
+		vim.keymap.set("v", "<TAB>", function()
+			require("nvim-treesitter.incremental_selection").scope_incremental()
+		end, { desc = "Increment treesitter scope" })
+		vim.keymap.set("v", "<S-TAB>", function()
+			require("nvim-treesitter.incremental_selection").node_decremental()
+		end, { desc = "Decrement treesitter selection" })
 	end,
 }
